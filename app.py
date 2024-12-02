e9e3f271f69f4d07b2b09a2089d349ffdc35cdd2
mannuel190 <emmanueladegbola23@live.com>
2024-12-02 17:15:32 +0000

    added shebang to app.py

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 from flask import Flask, jsonify, request, render_template  # Import render_template
 from supabase import create_client, Client
 from flask_cors import CORS
