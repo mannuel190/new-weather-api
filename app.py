2e503230c2faaaa27de62d9fa1a4a941feecf514
mannuel190 <emmanueladegbola23@live.com>
2024-12-02 17:34:50 +0000

    attempt to make app.py excecutable by default... and scheduler

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+
 from flask import Flask, jsonify, request, render_template  # Import render_template
 from supabase import create_client, Client
 from flask_cors import CORS
